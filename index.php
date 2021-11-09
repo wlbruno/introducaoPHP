1dd0bc99d0c9cdec34196355a9d4d53ca67b67e9
Willian Bruno <50450402+wlbruno@users.noreply.github.com>
2021-11-09 17:20:19 -0300

    Create index.php
    Exemplo

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    echo "Hello DIO";
+
+?>
+
+
+<?php 
+    
+    $cursos = 30;
+    $pagina = "php";
+
+    //echo "Temos $cursos disponiveis";
+
+    if($site === "php"){
+	$cursos = 15;
+	echo "Temos $cursos disponíveis ";
+    }
+?>
